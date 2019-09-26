01370ab357b286b0f9f0a76f8bfb5347682b70c8
Jimmy Breseman <breseman9@gmail.com>
2019-09-25 20:51:33 -0500

    updated Database with recipes db

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,13 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+CREATE DATABASE recipes_db;
+USE recipes_db;
+CREATE TABLE users (
+    id int(14) auto_increment PRIMARY KEY,
+   userName varchar(250) not null,
+   password varchar(250) not null,
+   wins int(100),
+   losses int(100)
+);
