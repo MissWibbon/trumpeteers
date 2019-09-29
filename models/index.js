b704b581f36faa61263af15feaa3b83a01f78cbd
Michelle <wuebben.michelle@yahoo.com>
2019-09-28 20:09:23 -0500

    cleaned up index.js

--- models/index.js
+++ models/index.js
@@ -11,7 +11,6 @@ var db = {};
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 
-const axios = require("axios");
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
@@ -44,71 +43,5 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-/* function recipeApi() {
-  axios.get('https://api.spoonacular.com/recipes/random?apiKey=13a30efe431d4713aa2b0934b908feae')
-  .then(function (response) {
-    // handle success
-    
-    var results = response.data;
-    var recipe = response.data.recipes[0].title;
-    var image = response.data.recipes[0].image;
-    var servings = response.data.recipes[0].servings;
-    var ingredients = response.data.recipes[0].extendedIngredients;
-    var instructions = response.data.recipes[0].instructions.split(".");
-    var cookingDuration = response.data.recipes[0].readyInMinutes;
 
-
-   console.log(recipe);
-   console.log(image);
-   console.log(servings);
-   console.log(instructions);
-   console.log(ingredients);
-   console.log(cookingDuration);
-   console.log(results);
-    
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-}
-recipeApi();
-
-function incorrectRecipeApi() {
-  axios.get('https://api.spoonacular.com/recipes/random?apiKey=13a30efe431d4713aa2b0934b908feae')
-  .then(function (response) {
-    // handle success
-    
-    var results = response.data;
-    // var incorrectRecipe = response.data.recipes[0].title;
-    var incorrectIngredients = response.data.recipes[0].extendedIngredients;
-    var incorrectInstructions = response.data.recipes[0].instructions;
-    // var incorrectInstructions = response.data.recipes[0].instructions.split(".");
-
-
-    console.log(results);
-    // console.log(incorrectRecipe);
-    console.log(`
-    Incorrect Instructions
-    ___________________________________________________________________
-    ${incorrectInstructions}
-    
-    ___________________________________________________________________
-    `);
-    // console.log(incorrectIngredients);
-    
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
-}
-incorrectRecipeApi();
-*/
 module.exports = db;
