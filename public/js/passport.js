590e5924698709d000f6ffd6df7a75b51be28c8b
molly02 <molly_patterson@yahoo.com>
2019-09-30 15:35:16 -0500

    updated routes, mysql database, added new passport file

---
+++ public/js/passport.js
@@ -0,0 +1,17 @@
+console.log("passport is working");
+
+$(document).on("click", "#submitbutton", function(){
+    alert("tried to log in");
+
+    var username = $("#user").val().trim();
+    var password = $("#pass").val().trim();
+
+    var data = {
+        username : username,
+        password : password
+    };
+    alert(data);
+$.post("/login", data)
+alert("successful log in attempt");
+
+})
