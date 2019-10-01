ac715d55855075afdb3a8388eb4ab1b884f1294b
Jimmy Breseman <jimmybreseman9@hotmail.com>
2019-09-30 21:51:42 -0500

    corrected img and audio array imput

--- public/js/media.js
+++ public/js/media.js
@@ -1,53 +1,53 @@
 //  img template
-// { 'img': 'public/images/' }
+// { 'img': '/images/' }
 //  audio template
-// { 'audio': 'public/audio/' }
+// { 'audio': '/audio/' }
 
 var winImages = [
-  { 'img': 'public/images/GRaExcited.gif' },
-  { 'img': 'public/images/public/images/GRgordonGoat.jpg' },
-  { 'img': 'public/images/public/images/GRPeeringLeft.jpg' },
-  { 'img': 'public/images/public/images/GRSmilingOutside.png' },
+  { 'img': '/images/GRaExcited.gif' },
+  { 'img': '/images/public/images/GRgordonGoat.jpg' },
+  { 'img': '/images/public/images/GRPeeringLeft.jpg' },
+  { 'img': '/images/public/images/GRSmilingOutside.png' },
 ];
 function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
+
 var loseImages = [
-  { 'img': 'public/images/public/images/GRawHell.jpg' },
-  { 'img': 'public/images/public/images/GRCryingMan.jpg' },
-  { 'img': 'public/images/public/images/GRCutoffGirl.jpg' },
-  { 'img': 'public/images/public/images/GREyeRoll.gif' },
-  { 'img': 'public/images/public/images/GRFinesse.gif' },
-  { 'img': 'public/images/public/images/GRgetOut.jpg' },
-  { 'img': 'public/images/public/images/GRGetTheF@$&Out.gif' },
-  { 'img': 'public/images/public/images/GRHandsOnHead.jpg' },
-  { 'img': 'public/images/public/images/GRHeadSpinning.gif' },
-  { 'img': 'public/images/public/images/GROhFace.jpg' },
-  { 'img': 'public/images/public/images/GRWhySoDifficult.gif' },
-  { 'img': 'public/images/public/images/GRYelling.jpg' }
+  { 'img': '/images/public/images/GRawHell.jpg' },
+  { 'img': '/images/public/images/GRCryingMan.jpg' },
+  { 'img': '/images/public/images/GRCutoffGirl.jpg' },
+  { 'img': '/images/public/images/GREyeRoll.gif' },
+  { 'img': '/images/public/images/GRFinesse.gif' },
+  { 'img': '/images/public/images/GRgetOut.jpg' },
+  { 'img': '/images/public/images/GRGetTheF@$&Out.gif' },
+  { 'img': '/images/public/images/GRHandsOnHead.jpg' },
+  { 'img': '/images/public/images/GRHeadSpinning.gif' },
+  { 'img': '/images/public/images/GROhFace.jpg' },
+  { 'img': '/images/public/images/GRWhySoDifficult.gif' },
+  { 'img': '/images/public/images/GRYelling.jpg' }
 ];
 function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 var winAudio = [
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
 ];
 
 function getRandomItem(arr) {
@@ -55,27 +55,21 @@ function getRandomItem(arr) {
 }
 
 var loseAudio = [
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-  { 'audio': 'public/audio/' },
-
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
+  { 'audio': '/audio/' },
 ];
 
 function getRandomItem(arr) {
