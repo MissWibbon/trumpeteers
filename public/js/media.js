fc90438474e7254f1caaf3a9b57c8ce1aa51b117
Jimmy Breseman <jimmybreseman9@hotmail.com>
2019-10-02 21:49:55 -0500

    deleted duplicate win and lose img arrays

--- public/js/media.js
+++ public/js/media.js
@@ -1,5 +1,3 @@
-
-
 var winImages = [
   { 'img': '/images/GRaExcited.gif' },
   { 'img': '/images/public/images/GRgordonGoat.jpg' },
@@ -81,30 +79,5 @@ function getRandomItem(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-var winImages = [
-   { 'img': '/images/GRaExcited.gif' },
-   { 'img': '/images/GRgordonGoat.jpg' },
-   { 'img': '/images/GRPeeringLeft.jpg' },
-   { 'img': '/images/GRSmilingOutside.png'}
- ];
-   function getRandomItem(arr) {
-   return arr[Math.floor(Math.random() * arr.length)];
- }
- var loseImages = [
-   { 'img': 'public/images/public/images/GRawHell.jpg'},
-   { 'img': 'public/images/public/images/GRCryingMan.jpg' },
-   { 'img': 'public/images/public/images/GRCutoffGirl.jpg' },
-   { 'img': 'public/images/public/images/GREyeRoll.gif' },
-   { 'img': 'public/images/public/images/GRFinesse.gif' },
-   { 'img': 'public/images/public/images/GRgetOut.jpg' },
-   { 'img': 'public/images/public/images/GRGetTheF@$&Out.gif' },
-   { 'img': 'public/images/public/images/GRHandsOnHead.jpg' },
-   { 'img': 'public/images/public/images/GRHeadSpinning.gif' },
-   { 'img': 'public/images/public/images/GROhFace.jpg' },
-   { 'img': 'public/images/public/images/GRWhySoDifficult.gif' },
-   { 'img': 'public/images/public/images/GRYelling.jpg' }
- ];
-   function getRandomItem(arr) {
-   return arr[Math.floor(Math.random() * arr.length)];
- }
+
 
